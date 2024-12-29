6b7dfc5cea332b88c917db0075cf3beeeb8a2658
manny <bobokeke0@gmail.com>
2024-12-29 07:04:20 +0000

    updates

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,11 +5,11 @@ module.exports = {
         script: "yarn start",
         cwd: "./parse-server",
       },
-      {
-        name: "react",
-        script: "yarn start --host",
-        cwd: "./react",
-      },
+      // {
+      //   name: "react",
+      //   script: "yarn start --host",
+      //   cwd: "./react",
+      // },
       // {
       //   name: "nextjs",
       //   script: "yarn start",
